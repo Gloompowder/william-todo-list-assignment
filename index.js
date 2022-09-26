70155f34efea0a720662a702445ece8ce7da779d
William  Lin <williamlin@williams-mbp.myfiosgateway.com>
2022-09-26 00:51:22 -0400

    added delete

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const dashboard = document.querySelector(".dashboard");
 const submit = document.querySelector(".submit-button");
 const inputBar = document.querySelector(".input-field");
 const inputForm = document.querySelector(".input-form");
+const deleteButton = document.querySelectorAll("delete");
+
+var todoArr = [];
 
 const fetchTodos = fetch(url)
 .then(res=>res.json())
@@ -17,17 +20,18 @@ function getTodos(){
             `<form key=${todo.id} class="todo-item">
             <div class="todo-item-text">${todo.title}
             <div/>
-            <input class = "delete-button" type ="submit" value = "trash">
+            <input onclick="handleDelete(${todo.id})" class = "delete-button" type ="button" value = "trash">
             </form>`
             :`<form key=${todo.id} class="todo-item">
             <div class="todo-item-text">${todo.title}
             <div/>
-            <input class="edit" type ="submit" value="edit">
-            <input class="delete" type ="submit" value = "trash">
+            <input onclick="handleUpdate(${todo.id})" class="edit" type ="submit" value="edit">
+            <input onclick="handleDelete(${todo.id})" class="delete" type ="button" value = "trash">
             </form>`
         }).join("");
-        dashboard.innerHTML = todoData;})};
-        getTodos();
+    todoArr = todoData;
+    dashboard.innerHTML = todoArr;})};
+getTodos();
 
         // handling inputs
 function handleInput(e){
@@ -45,12 +49,9 @@ function createTodo(){
         body: JSON.stringify({ title: inputBar.value, completed:false})
     };
     fetch(url, requestOptions)
-    .then(fetch(url)
-    .then(res=>res.json()
-    .then(
-        data=>console.log(data)
-    ))
-    );
+    .then(res=>res.json())
+    .then(data=>todoArr<<(data.join("")))
+    .then(dashboard.innerHTML = todoArr);
 }
 
 function handleSubmit(e){
@@ -60,6 +61,41 @@ function handleSubmit(e){
 
 inputForm.addEventListener("submit", handleSubmit);
 
+
+// DELETE call
+
+function handleDelete(id){
+
+fetch(url+`/${id}`, { method: 'DELETE' })
+    .then(() => console.log(`id at : ${id} deleted.`))
+};
+
+
+// POST completed call 
+
+function handlePost(id){
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ completed: true })
+    };
+    fetch(url+`/${id}`, requestOptions)
+    .then(res=>res.json())
+    .then(data=>console.log(data, `id at : ${id} completed.`));
+    };
+    
+// PUT edit task call
+
+// function handlePost(id, changes){
+//     const requestOptions = {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ title: changes})
+//     };
+//     fetch(url+`/${id}`, requestOptions)
+//     .then(res=>res.json())
+//     .then(data=>console.log(data, `id at : ${id} completed.`));
+//     };
 
 
 
